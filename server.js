d29c21b0acfdcd871a0ec2a9e8e4deacc97ceed5
Saiful Islam Sajon <saifulislamsajon@gmail.com>
2020-07-08 16:18:43 +0600

    MongoDB database has connected

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));

2be6597f81bd464ca14054dbbf211a99ad7ed4f4
Saiful Islam Sajon <saifulislamsajon@gmail.com>
2020-07-10 00:19:30 +0600

    edit and update contact has been done

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,6 +1,23 @@
-import React, {useState, useContext} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import ContactContext from '../../context/contact/contactContext';
 const ContactForm = () => {
+
+    // Initialize context
+    const contactContext = useContext(ContactContext);
+    const { addContact, current, clearCurrent, updateContact } = contactContext;
+    
+    useEffect( () => {
+        if(current !== null) {
+            setContact(current);
+        } else {
+            setContact({
+                name: '',
+                email: '',
+                phone: '',
+                type: 'personal'
+            });
+        }
+    }, [contactContext, current])
 
     const [contact, setContact] = useState({
         name: '',
@@ -12,8 +29,8 @@ const ContactForm = () => {
     // Destructering state value
     const {name, email, phone, type} = contact;
     
-    // Initialize context
-    const contactContext = useContext(ContactContext);
+
+
 
     /*
     * @param Event
@@ -29,18 +46,32 @@ const ContactForm = () => {
     */
     const onSubmit = e => {
         e.preventDefault();
-        contactContext.addContact(contact);
-        setContact({
-            name: '',
-            email: '',
-            phone: '',
-            type: 'personal'
-        });
+
+        if(!current) {
+            addContact(contact);
+        } else {
+            updateContact(contact);
+        }
+
+
+        // setContact({
+        //     name: '',
+        //     email: '',
+        //     phone: '',
+        //     type: 'personal'
+        // });
+    }
+
+    /*
+    * Clear Current Contact from form
+    */
+    const clearAll = () => {
+        clearCurrent();
     }
 
     return (
         <form onSubmit={onSubmit}>
-            <h2 className='text-primary'>Add Contact</h2>
+            <h2 className='text-primary'>{current ? 'Edit Contact' : 'Add Contact'}</h2>
             
             <input type="text" placeholder="Name" name="name" value={name} onChange={onChange} />
             <input type="email" placeholder="Email"name="email"value={email} onChange={onChange} />
@@ -52,7 +83,10 @@ const ContactForm = () => {
             <input type="radio" name="type" value="professional" id="pro" onChange={onChange}/> <label htmlFor="pro">Professional</label>
 
             <div>
-                <input type="submit" value="Add Contact" className="btn btn-primary"/>
+                <input type="submit" value={current ? 'Update Contact' : 'Add Contact'} className="btn btn-primary btn-block"/>
+                {current && (<div>
+                    <input type="submit" value="Clear Contact" className="btn btn-light btn-block" onClick={clearAll}/>
+                </div>)}
             </div>
 
         </form>
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -67,7 +67,9 @@ const ContactState = props => {
     }
 
     // Update Contact
-
+    const updateContact = (contact) => {
+        dispatch({type: UPDATE_CONTACT, payload: contact});
+    }
     // Filter Contacts
 
     // Clear Filter
@@ -92,7 +94,8 @@ const ContactState = props => {
             addContact,
             deleteContact,
             setCurrent,
-            clearCurrent
+            clearCurrent,
+            updateContact
         }}>
             {props.children}
         </ContactContext.Provider>
